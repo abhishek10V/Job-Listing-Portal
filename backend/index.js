573c242fe9a02c96e994a288c483d464faad42a2
Shashank Agarwal <162678882+shashankagarwalgit@users.noreply.github.com>
2024-08-21 12:30:16 +0530

    login sigup complete and fix minor bugs

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 
 // api's
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({});
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/JobPortalDB");
+        await mongoose.connect(process.env.DB_URL);
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
